e3f1e402035248da68219033c657baf2d3f8a805
hazzot <74054812+hazzot@users.noreply.github.com>
2023-11-15 16:30:37 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,135 +1,190 @@
-var theCount;
-var alarm = document.getElementById("alarm");
-var panel = document.getElementById("panel");
-var turnOff = document.getElementById("turn-off");
-var turnOffHor = document.getElementById("closing");
-var detonate = document.getElementById("detonate");
-var again = document.getElementById("again");
-var panel2 = document.getElementById("panel2");
-alarm.volume = 0.5; //volume level
+let particles = [];
+let microparticles = [];
 
-var time = document.getElementById("time");
-function showCountDown() {
-	time.innerText = time.innerText - 1;
-	if (time.innerText == 0) {
-		clearInterval(theCount);
-		time.classList.add("crono");
-		abort.classList.add("hide");
-        no.classList.add("hide");
-        again.classList.add("hide");
-        
-		
-        detonate.classList.add("show");
-		setTimeout(function () {
-			turnOff.classList.add("close");
-			turnOffHor.classList.add("close");
-			reload.classList.add("show");
-			alarm.pause();
-		}, 1500);
-	}
-   
+const c1 = createCanvas({
+  width: $(window).width(),
+  height: $(window).height(),
+});
+
+const tela = c1.canvas;
+const canvas = c1.context;
+
+// $("body").append(tela);
+$("body").append(c1.canvas);
+
+class Particle1 {
+  constructor(canvas) {
+    this.random = Math.random();
+    this.random1 = Math.random();
+    this.random2 = Math.random();
+    this.progress = 0;
+    this.canvas = canvas;
+    this.life = 1000 + Math.random() * 3000;
+
+    this.x =
+      $(window).width() / 2 + (Math.random() * 20 - Math.random() * 20);
+    this.y = $(window).height();
+    this.s = 2 + Math.random();
+    this.w = $(window).width();
+    this.h = $(window).height();
+    this.direction = this.random > 0.5 ? -1 : 1;
+    this.radius = 1 + 3 * this.random;
+    this.color = "#ff417d";
+
+    this.ID = setInterval(
+      function() {
+        microparticles.push(
+          new microParticle(c1.context, {
+            x: this.x,
+            y: this.y,
+          })
+        );
+      }.bind(this),
+      this.random * 20
+    );
+
+    setTimeout(
+      function() {
+        clearInterval(this.ID);
+      }.bind(this),
+      this.life
+    );
+  }
+
+  render() {
+    this.canvas.beginPath();
+    this.canvas.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    // this.canvas.lineWidth = 2;
+    this.canvas.shadowOffsetX = 0;
+    this.canvas.shadowOffsetY = 0;
+    // this.canvas.shadowBlur = 6;
+    this.canvas.shadowColor = "#000000";
+    this.canvas.fillStyle = this.color;
+    this.canvas.fill();
+    this.canvas.closePath();
+  }
+
+  move() {
+    this.x -=
+      this.direction *
+      Math.sin(this.progress / (this.random1 * 430)) *
+      this.s;
+    this.y -= Math.cos(this.progress / this.h) * this.s;
+
+    if (this.x < 0 || this.x > this.w - this.radius) {
+      clearInterval(this.ID);
+      return false;
+    }
+
+    if (this.y < 0) {
+      clearInterval(this.ID);
+      return false;
+    }
+    this.render();
+    this.progress++;
+    return true;
+  }
 }
 
-var cover = document.getElementById("cover");
-cover.addEventListener("click", function () {
-	if (this.className == "box") this.classList.add("opened");
-	else this.classList.remove("opened");
-});
+class microParticle {
+  constructor(canvas, options) {
+    this.random = Math.random();
+    this.random1 = Math.random();
+    this.random2 = Math.random();
+    this.progress = 0;
+    this.canvas = canvas;
 
-var btn = document.getElementById("activate");
-activate.addEventListener("click", function () {
-	this.classList.add("pushed");
-	alarm.load();
-	alarm.currentTime = 10.1;
-	alarm.play();
-	setTimeout(function () {
-		panel.classList.add("show");
-		theCount = setInterval(showCountDown, 1000);
-		alarm.load();
-		alarm.play();
-	}, 500);
-});
+    this.x = options.x;
+    this.y = options.y;
+    this.s = 2 + Math.random() * 3;
+    this.w = $(window).width();
+    this.h = $(window).height();
+    this.radius = 1 + this.random * 0.5;
+    this.color = "#4EFCFE"; //this.random > .5 ? "#a9722c" : "#FFFED7"
+  }
 
-var abort = document.getElementById("abort");
-abort.addEventListener("click", function () {
-	btn.classList.remove("pushed");
-	panel.classList.remove("show");
-	clearInterval(theCount);
-	time.innerText = 9;
-	alarm.pause();
-	alarm.currentTime = 10;
-	alarm.play();
-   
-});
+  render() {
+    this.canvas.beginPath();
+    this.canvas.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    this.canvas.lineWidth = 2;
+    this.canvas.fillStyle = this.color;
+    this.canvas.fill();
+    this.canvas.closePath();
+  }
 
-var no = document.getElementById("no");
-no.addEventListener("click", function () {
-	btn.classList.remove("pushed");
-	panel.classList.remove("show");
-	clearInterval(theCount);
-	time.innerText = 9;
-	alarm.pause();
-	alarm.currentTime = 10;
-	alarm.play();
-    panel2.classList.add("show");
-});
+  move() {
+    this.x -=
+      Math.sin(this.progress / (100 / (this.random1 - this.random2 * 10))) *
+      this.s;
+    this.y += Math.cos(this.progress / this.h) * this.s;
 
-var again = document.getElementById("again");
-again.addEventListener("click", function () {
-	btn.classList.remove("pushed");
-	panel2.classList.remove("show");
-    no.classList.remove("pushed");
-	clearInterval(theCount);
-	time.innerText = 9;
-	alarm.pause();
-	alarm.currentTime = 10;
-	alarm.play();
-    
-    
-    btn.classList.add("pushed");
-	alarm.load();
-	alarm.currentTime = 10.1;
-	alarm.play();
-	setTimeout(function () {
-		panel.classList.add("show");
-		theCount = setInterval(showCountDown, 1000);
-		alarm.load();
-		alarm.play();
-	}, 500);
-});
+    if (this.x < 0 || this.x > this.w - this.radius) {
+      return false;
+    }
 
+    if (this.y > this.h) {
+      return false;
+    }
+    this.render();
+    this.progress++;
+    return true;
+  }
+}
 
+var random_life = 1000;
 
+setInterval(
+  function() {
+    particles.push(new Particle1(canvas));
+    random_life = 2000 * Math.random();
+  }.bind(this),
+  random_life
+);
 
-var reload = document.getElementById("restart");
-reload.addEventListener("click", function () {
-    panel.classList.remove("show");
-	turnOff.classList.remove("close");
-	turnOffHor.classList.remove("close");
-	abort.classList.remove("hide");
-    no.classList.remove("hide");
-    
-    
-	
-    detonate.classList.remove("show");
-	cover.classList.remove("opened");
-	btn.classList.remove("pushed");
-	this.classList.remove("show");
-	time.classList.remove("crono");
-	time.innerText = 9;
-});
+function clear() {
+  let grd = canvas.createRadialGradient(
+    tela.width / 2,
+    tela.height / 2,
+    0,
+    tela.width / 2,
+    tela.height / 2,
+    tela.width
+  );
+  grd.addColorStop(0, "rgba(20,20,20,1)");
+  grd.addColorStop(1, "rgba(0,0,0,0)");
+  // Fill with gradient
+  canvas.globalAlpha = 0.16;
+  canvas.fillStyle = grd;
+  canvas.fillRect(0, 0, tela.width, tela.height);
+}
 
-setTimeout(function () {
-	cover.classList.remove("opened");
-}, 100);
+function blur(ctx, canvas, amt) {
+  // ctx.filter = `blur(${amt}px)`
+  // ctx.drawImage(canvas, 0, 0)
+  // ctx.filter = 'none'
+}
 
-var mute = document.getElementById("mute");
-mute.addEventListener("click", function () {
-	if (this.className == "muted") {
-		alarm.muted = false;
-		this.classList.remove("muted");
-	} else {
-		alarm.muted = true;
-		this.classList.add("muted");
-	}
-});
+function update() {
+  clear();
+  particles = particles.filter(function(p) {
+    return p.move();
+  });
+  microparticles = microparticles.filter(function(mp) {
+    return mp.move();
+  });
+  requestAnimationFrame(update.bind(this));
+}
+
+function createCanvas(properties) {
+  let canvas = document.createElement("canvas");
+  canvas.width = properties.width;
+  //   canvas.style.zIndex = 999;
+  canvas.height = properties.height;
+  let context = canvas.getContext("2d");
+  return {
+    canvas: canvas,
+    context: context,
+  };
+}
+
+update();
